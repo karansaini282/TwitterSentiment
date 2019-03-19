41a7b3e882ad9b8bdba624f5da54d7641963d5e2
Karan Saini <karansaini282@yahoo.com>
2019-03-19 18:04:47 -0400

    Update TwitterConsumer.java

--- TwitterConsumer.java
+++ TwitterConsumer.java
@@ -38,62 +38,69 @@ import java.util.Date;
 import scala.Tuple2;
 
 public class TwitterConsumer implements Runnable{
-    private static int sentSumPos;
-    private static int sentSumNeg;
-    private static Map<String,Integer> dict;
-    private String searchQuery;
-    private String[] keywords;
+	private static int sentSumPos;
+	private static int sentSumNeg;
+	private static Map<String,Integer> dict;
+	private String searchQuery;
+	private String[] keywords;
 
-    public TwitterConsumer()
-    {
-    	try
-    	{
-	    	sentSumPos = 0;
-	    	sentSumNeg = 0;
-	        dict = new HashMap<String,Integer>();
-	        
-	    	File dir = new File(".");
-	    	File fin = new File(dir.getCanonicalPath() + File.separator + "src/good.txt");
-	    	FileInputStream fis = new FileInputStream(fin);
-	    	BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-	     
-	    	String line = null;
-	    	while ((line = br.readLine()) != null) {
-	    		dict.put(line,1);
-	    	}
-	     
-	    	br.close();
+	public TwitterConsumer()
+	{
+	try
+	{
+		sentSumPos = 0;
+		sentSumNeg = 0;
+		dict = new HashMap<String,Integer>();
+
+		File dir = new File(".");
+		File fin = new File(dir.getCanonicalPath() + File.separator + "src/good.txt");
+		FileInputStream fis = new FileInputStream(fin);
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			dict.put(line,1);
+		}
+
+		br.close();
+
+		File dir2 = new File(".");
+		File fin2 = new File(dir2.getCanonicalPath() + File.separator + "src/bad.txt");
+		FileInputStream fis2 = new FileInputStream(fin2);
+		BufferedReader br2 = new BufferedReader(new InputStreamReader(fis2));
+
+		String line2 = null;
+		while ((line2 = br2.readLine()) != null) {
+			dict.put(line2,-1);
+		}
+
+		br2.close();
+	}
+	catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+	}
+    
+	public static final Function<MessageAndMetadata<String, String>, Tuple2> MessageAndMetadataFunction = new Function<MessageAndMetadata<String, String>, Tuple2>()
+	{
+	@Override
+	public Tuple2<String,String> call(MessageAndMetadata<String, String> v1)
+		throws Exception {            	
+	    return new Tuple2(v1.topic(),v1.message());
+	}
+	};
+ 
+	public static final PairFunction<Tuple2,String,String> createPair = new PairFunction<Tuple2,String,String>()
+	{
+		@Override
+		public Tuple2<String,String> call(Tuple2 a)
+		{	             
+			return new Tuple2<String,String>(a._1.toString(),a._2.toString());
+		}
+	};
 	
-	    	File dir2 = new File(".");
-	    	File fin2 = new File(dir2.getCanonicalPath() + File.separator + "src/bad.txt");
-	    	FileInputStream fis2 = new FileInputStream(fin2);
-	    	BufferedReader br2 = new BufferedReader(new InputStreamReader(fis2));
-	    	
-	    	String line2 = null;
-	    	while ((line2 = br2.readLine()) != null) {
-	    		dict.put(line2,-1);
-	    	}
-	     
-	    	br2.close();
-    	}
-    	catch(Exception e)
-    	{
-    		e.printStackTrace();
-    	}
-    }
-    
-    public static final Function<MessageAndMetadata<String, String>, Tuple2> MessageAndMetadataFunction = new Function<MessageAndMetadata<String, String>, Tuple2>()
-    {
-
-        @Override
-        public Tuple2<String,String> call(MessageAndMetadata<String, String> v1)
-                throws Exception {            	
-            return new Tuple2(v1.topic(),v1.message());
-        }
-
-    };
-    
-    public static final Function<String,Tuple2<Integer,Integer>> mapFunction=new Function<String,Tuple2<Integer,Integer>>()
+	public static final Function<String,Tuple2<Integer,Integer>> mapFunction = new Function<String,Tuple2<Integer,Integer>>()
 	{
 		@Override
 		public Tuple2<Integer,Integer> call(String tweet)
@@ -122,8 +129,17 @@ public class TwitterConsumer implements Runnable{
 			return new Tuple2<Integer,Integer>(sentSumPos,sentSumNeg);
 		}
 	};
+
+	public static final Function2<Tuple2<Integer,Integer>,Tuple2<Integer,Integer>,Tuple2<Integer,Integer>> reduceFunction = new Function2<Tuple2<Integer,Integer>,Tuple2<Integer,Integer>,Tuple2<Integer,Integer>>()
+	{
+		@Override
+		public Tuple2<Integer,Integer> call(Tuple2<Integer,Integer> a,Tuple2<Integer,Integer> b)
+		{			
+			return new Tuple2<Integer,Integer>(a._1+b._1,a._2+b._2);
+		}
+	};
 	
-    public static final Function<Tuple2<String,Tuple2<Integer,Integer>>,Row> rowFunction=new Function<Tuple2<String,Tuple2<Integer,Integer>>,Row>()
+	public static final Function<Tuple2<String,Tuple2<Integer,Integer>>,Row> rowFunction = new Function<Tuple2<String,Tuple2<Integer,Integer>>,Row>()
 	{
 		@Override
 		public Row call(Tuple2<String,Tuple2<Integer,Integer>> a)
@@ -133,95 +149,82 @@ public class TwitterConsumer implements Runnable{
 		}
 	};
 	
-    public static final PairFunction<Tuple2,String,String> createPair=new PairFunction<Tuple2,String,String>()
-	{
-		@Override
-		public Tuple2<String,String> call(Tuple2 a)
-		{	             
-			return new Tuple2<String,String>(a._1.toString(),a._2.toString());
-		}
-	};
+
 	
-    public static final Function2<Tuple2<Integer,Integer>,Tuple2<Integer,Integer>,Tuple2<Integer,Integer>> reduceFunction=new Function2<Tuple2<Integer,Integer>,Tuple2<Integer,Integer>,Tuple2<Integer,Integer>>()
+
+    
+	public void run()
 	{
-		@Override
-		public Tuple2<Integer,Integer> call(Tuple2<Integer,Integer> a,Tuple2<Integer,Integer> b)
-		{			
-			return new Tuple2<Integer,Integer>(a._1+b._1,a._2+b._2);
+		try{
+
+		SparkConf conf = new SparkConf()
+			.setAppName("kafka-consumer")
+			.setMaster("local");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaStreamingContext ssc = new JavaStreamingContext(sc, new Duration(2000));
+
+		Map<String, String> kafkaParams = new HashMap<>();
+		kafkaParams.put("metadata.broker.list", "localhost:9092");
+
+		Properties connectionProperties = new Properties();
+		connectionProperties.put("user", "root");
+		connectionProperties.put("password", "admin");
+		SQLContext sqlContext = new SQLContext(sc);
+		// The schema is encoded in a string
+		String schemaString = "topic sentimentPos sentimentNeg timestamp";
+
+		// Generate the schema based on the string of schema
+		List<StructField> fields = new ArrayList<>();
+		for (String fieldName : schemaString.split(" ")) {
+			StructField field = DataTypes.createStructField(fieldName, DataTypes.StringType, true);
+			fields.add(field);
 		}
-	};
+		StructType schema = DataTypes.createStructType(fields);
+
+		Properties prop = new Properties();
+		InputStream input = null;
+		input = getClass().getClassLoader().getResourceAsStream("twitter.properties");
+		prop.load(input);
+		searchQuery=prop.getProperty("searchQuery");
+		keywords=searchQuery.split(" OR ");
+
+		Map<TopicAndPartition, Long> topicAndPartition = new HashMap();
+		for(String keyword:keywords)
+		{
+			topicAndPartition.put(new TopicAndPartition(keyword, 0), 1L);
+		}        
+
+		KafkaUtils.createDirectStream(ssc, String.class, String.class, StringDecoder.class, StringDecoder.class, Tuple2.class, kafkaParams, topicAndPartition, MessageAndMetadataFunction)
+		.mapToPair(createPair)
+		.mapValues(mapFunction).reduceByKey(reduceFunction).map(rowFunction).foreachRDD(rdd -> {
+			Dataset<Row> df = sqlContext.createDataFrame(rdd,schema).na().drop();                    
+			df.select("topic","sentimentPos","sentimentNeg","timestamp").write().mode("append").jdbc("jdbc:mysql://localhost:3306/db1?autoReconnect=true&useSSL=false", "db1.twitterSentiment", connectionProperties);
+			rdd.foreach(record -> {
+			});            
+		});
+
+		ssc.start();
+		ssc.awaitTermination();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+        }
     
-    public void run(){
-        try{
-        	  
-        SparkConf conf = new SparkConf()
-                .setAppName("kafka-consumer")
-                .setMaster("local");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaStreamingContext ssc = new JavaStreamingContext(sc, new Duration(2000));
-        
-        Map<String, String> kafkaParams = new HashMap<>();
-        kafkaParams.put("metadata.broker.list", "localhost:9092");
-
-        Properties connectionProperties = new Properties();
-        connectionProperties.put("user", "root");
-        connectionProperties.put("password", "admin");
-        SQLContext sqlContext = new SQLContext(sc);
-        // The schema is encoded in a string
-        String schemaString = "topic sentimentPos sentimentNeg timestamp";
-
-        // Generate the schema based on the string of schema
-        List<StructField> fields = new ArrayList<>();
-        for (String fieldName : schemaString.split(" ")) {
-        	StructField field = DataTypes.createStructField(fieldName, DataTypes.StringType, true);
-        	fields.add(field);
-        }
-        StructType schema = DataTypes.createStructType(fields);
-        
-    	Properties prop = new Properties();
-    	InputStream input = null;
-        input = getClass().getClassLoader().getResourceAsStream("twitter.properties");
-        prop.load(input);
-        searchQuery=prop.getProperty("searchQuery");
-        keywords=searchQuery.split(" OR ");
-        
-        Map<TopicAndPartition, Long> topicAndPartition = new HashMap();
-        for(String keyword:keywords)
-        {
-        	topicAndPartition.put(new TopicAndPartition(keyword, 0), 1L);
-        }        
-
-        KafkaUtils.createDirectStream(ssc, String.class, String.class, StringDecoder.class, StringDecoder.class, Tuple2.class, kafkaParams, topicAndPartition, MessageAndMetadataFunction)
-	.mapToPair(createPair)
-        .mapValues(mapFunction).reduceByKey(reduceFunction).map(rowFunction).foreachRDD(rdd -> {
-         	Dataset<Row> df = sqlContext.createDataFrame(rdd,schema).na().drop();                    
-         	df.select("topic","sentimentPos","sentimentNeg","timestamp").write().mode("append").jdbc("jdbc:mysql://localhost:3306/db1?autoReconnect=true&useSSL=false", "db1.twitterSentiment", connectionProperties);
-         	rdd.foreach(record -> {
-            	});            
-        });
-        
-        ssc.start();
-        ssc.awaitTermination();
-        }
-        catch(Exception e)
-        {
-        	e.printStackTrace();
-        }
-    }
-    
-    public static void main(String[] args) 
-    {
-    	try
+	public static void main(String[] args) 
 	{
-	        TwitterConsumer obj = new TwitterConsumer();
-	        Thread t = new Thread(obj);
-	        t.start();
-	        t.join();
-    	}
-    	catch(Exception e)
-    	{
-    		e.printStackTrace();
-    	}
-    	
-    }
+		try
+		{
+			TwitterConsumer obj = new TwitterConsumer();
+			Thread t = new Thread(obj);
+			t.start();
+			t.join();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
 }
